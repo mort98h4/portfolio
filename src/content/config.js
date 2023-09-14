eae8e4183bf215eaf54f54b75ae98eb8dd78578f
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-09-14 10:29:18 +0200

    Expanded the schema of project entries

--- src/content/config.js
+++ src/content/config.js
@@ -5,12 +5,22 @@ const projects = defineCollection({
     schema: z.object({
         id: z.number(),
         title: z.string(),
+        body: z.string(),
         urls: z.array(
-                z.object({
-                    title: z.string(),
-                    href: z.string()
-                })
-            ),
+            z.object({
+                title: z.string(),
+                href: z.string()
+            })
+        ),
+        repositories: z.array(
+            z.object({
+                name: z.string(),
+                href: z.string()
+            })
+        ),
+        technologies: z.array(
+            z.string()
+        ),
         image: z.string().optional(),
     })
 });
