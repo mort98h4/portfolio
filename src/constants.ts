7f370fb6fc0e3350364aca5bc2cbacbb072b6150
Morten Schou Gross <mgr@dst.dk>
2025-07-02 09:42:56 +0200

    added constants for input validation

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const fullnameMin = 5;
+export const fullnameMax = 50;
+export const phoneRegex = /^(?:\+45\s?)?(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2})$/;
+export const phoneRegexStr = "^(?:\\+45\\s?)?(?:\\d{2}\\s?\\d{2}\\s?\\d{2}\\s?\\d{2})$";
+export const messageMin = 2;
+export const messageMax = 500;
