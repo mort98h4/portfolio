f7756fafec3efc8d821a2507e68bd959f853a074
Morten Schou Gross <mgr@dst.dk>
2025-07-08 15:27:11 +0200

    add environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_EMAIL_API_URL_PROD: string;
+  readonly PUBLIC_EMAIL_API_URL_DEV: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
