2e9b590fdb2023af0a392c6ffeac7b90c6ff4840
Morten Schou Gross <mgr@dst.dk>
2025-08-04 10:01:23 +0200

    moved navigation component to own folder + moved navLinks/navigation to its own file

---
+++ src/content/navigation.js
@@ -0,0 +1,58 @@
+export const navigation = {
+    da: {
+        links: [
+            {
+                href: '#profile',
+                title: 'Profil'
+            },
+            {
+                href: '#experience',
+                title: 'Erfaring'
+            },
+            {
+                href: '#projects',
+                title: 'Projekter'
+            },
+            {
+                href: '#contact',
+                title: 'Kontakt'
+            }
+        ],
+        togglers: {
+            language: {
+                title: 'Skift sprog'
+            },
+            darkMode: {
+                title: 'Slå mørk tilstand til/fra'
+            }
+        }
+    },
+    en: {
+        links: [
+            {
+                href: '#profile',
+                title: 'Profile'
+            },
+            {
+                href: '#experience',
+                title: 'Experience'
+            },
+            {
+                href: '#projects',
+                title: 'Projects'
+            },
+            {
+                href: '#contact',
+                title: 'Contact'
+            }
+        ],
+        togglers: {
+            language: {
+                title: 'Change language'
+            },
+            darkMode: {
+                title: 'Toggle dark mode'
+            }
+        }
+    }
+}
