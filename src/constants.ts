8617723dcf73b86ed8f640f052b4cd09d909b0eb
Morten Schou Gross <mgr@dst.dk>
2025-08-04 14:03:51 +0200

    moved language constants to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+export const mainLang = 'en';
+export const supportedLangs = ['da', 'en'];
+
 export const fullnameMin = 5;
 export const fullnameMax = 50;
 export const phoneRegex = /^(?:\+45\s?)?(?:\d{2}\s?\d{2}\s?\d{2}\s?\d{2})$/;
