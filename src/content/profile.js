50d01c71a3d1989d4f66113253d3dde1e5cc14ec
Morten Schou Gross <mgr@dst.dk>
2025-08-05 10:00:16 +0200

    implemented localization on the profile section

---
+++ src/content/profile.js
@@ -0,0 +1,44 @@
+export const profile = {
+    da: {
+        sectionHeader: 'Profil',
+        introduction: {
+            header: 'Hej, mit navn er Morten.',
+            subheader: 'Jeg er en full stack webudvikler, som er bosat i København.',
+            paragraph: 'Jeg er en webudvikler, der har fokus på at skabe brugervenlige, skalérbare og vedligeholdelsesvenlige webapplikationer. Jeg arbejder både med frontend og backend, og har erfaring med at udvikle responsive brugergrænseflader, skræddersyede CMS-løsninger og API-integrationer. Jeg har erfaring med teknologier som JavaScript, .NET, Python og SQL, og er vant til at benytte moderne biblioteker som Tailwind CSS, Angular og Astro. Jeg trives i agile miljøer (Scrum/Kanban) og bidrager aktivt til samarbejdet i tværfaglige teams for at føre digitale løsninger fra idé til færdigt produkt.'
+        },
+        personal: {
+            header: 'Personligt',
+            paragraph: 'Jeg er opvokset i Hvidovre og bor i dag på Nørrebro sammen med min hustru og vores hund, Preben. I mit personlige liv, bruger jeg gerne tid på mine hobbyer – jeg nyder at bygge Lego Star Wars-sæt, tage på stadion og se fodbold, samt at spille klassiske Pokémon-spil fra barndommen. Jeg værdsætter både kreativ fordybelse og fællesskab, og det afspejler sig i min tilgang til både arbejde og fritid.'
+        },
+        contact: {
+            header: 'Lad os tales ved!',
+            subheader: 'Du er velkommen til at kontakte mig via følgende kanaler.',
+            paragraph: '... eller benyt ',
+            link: {
+                href: '#contact',
+                text: 'kontaktformularen' 
+            }
+        }
+    },
+    en: {
+        sectionHeader: 'Profile',
+        introduction: {
+            header: 'Hi, I\'m Morten.',
+            subheader: 'I\'m a full stack web developer based in Copenhagen.',
+            paragraph: 'I\'m a web developer with a strong focus on building user-centric, scalable, and maintainable web solutions. I have experience in both frontend and backend development, with hands-on involvement in designing and implementing responsive web interfaces, custom CMS platforms, and API integrations. With a proven ability to deliver solutions across the full stack using technologies like JavaScript, .NET, Python, and SQL, along with modern tools such as Tailwind CSS, Angular, and Astro. I\'m comfortable working in Agile environments (Scrum/Kanban) and collaborating with cross-functional teams to bring digital products from concept to deployment.'
+        },
+        personal: {
+            header: 'Personal life',
+            paragraph: 'I grew up in Hvidovre and now live in Nørrebro with my wife and our dog, Preben. Outside of web development, I enjoy spending my time on hobbies that mix creativity and nostalgia — building Lego Star Wars sets, and playing classic retro Pokémon games. In addition I enjoy watching football at the stadium with friends, and attending festivals in the summer.I value both focused creativity and shared experiences, which also reflects in how I approach my work and daily life.'
+        },
+        contact: {
+            header: 'Lets get in touch!',
+            subheader: 'Feel free to reach out through the following channels.',
+            paragraph: '... or use the ',
+            link: {
+                href: '#contact',
+                text: 'contact form' 
+            }
+        }
+    }
+}
